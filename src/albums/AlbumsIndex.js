3ed10a6ba6a76f498a420159c304ef12c44846f7
mdipietr0 <dipietro.michael@gmail.com>
2018-10-07 13:03:52 -0400

    Add UI styling to AlbumsIndex
    - Create AlbumsIndex.scss
    - On :hover image transitions to transform scale x1.5
    - Change thumbnail to be div with a background-image property
    - Add text to thumb with text-shadow property for readability

--- src/albums/AlbumsIndex.js
+++ src/albums/AlbumsIndex.js
@@ -1,74 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 
-const AlbumsIndex = () => (
-  <div className="container-fluid mt-5">
-    <div className="d-flex flex-wrap justify-content-center">
-      <div className="">
-        <img className='mb-auto p-2' width='200px;' src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
+import './AlbumsIndex.scss'
+
+class AlbumsIndex extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {}
+  }
+
+  onHover = () => {
+    consoe.log('hover')
+  }
+
+  render () {
+
+    const Albums = () => {
+      const albums = []
+      for (let i = 0; i < 21; i++){
+        albums.push(
+          <div
+            key={i}
+            onHover={this.onHover}
+            className="album-thumb mb-auto m-1 display-box shadow"
+          >
+            <h6 className='mt-5 p-2 text-left text-white'>Abbey Road</h6>
+            <p className='p-2 mb-2 text-left text-white'>Abbey Road is the eleventh studio album by English rock band the Beatles, released on 26 September 1969 by Apple Records.</p>
+          </div>)
+      }
+      return albums
+    }
+
+    return (
+      <div className="container-fluid mt-5">
+        <div className="d-flex flex-wrap justify-content-center">
+          <Albums />
+        </div>
       </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;' src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;' src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;' src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-      <div className="">
-        <img className='mb-auto p-2' width='200px;'  src='https://img.discogs.com/jwmEhAbhbtJRNugbUvyehTPwJLs=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2498807-1425279296-1235.jpeg.jpg' />
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default AlbumsIndex
