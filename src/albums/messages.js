dfa622b0a93d0b68b28c39507214f52d520ce1b5
mdipietr0 <dipietro.michael@gmail.com>
2018-10-10 19:46:30 -0400

    Add removeFromWishlistFailure message

--- src/albums/messages.js
+++ src/albums/messages.js
@@ -1,4 +1,5 @@
 export default {
   addToWishlistSuccess: 'Added to wishlist.',
   addToWishlistFailure: 'Unable to add to wishlist. Album may already be in list.',
+  removeFromWishlistFailure: 'Unable to remove from wishlist.',
 }
