cd1c6c12be8bd2f139653db5f58f6f68dcc17d2a
mdipietr0 <dipietro.michael@gmail.com>
2018-10-10 11:55:30 -0400

    Remove console.log/comments from results render

--- src/albums/components/Results.js
+++ src/albums/components/Results.js
@@ -16,21 +16,11 @@ class Results extends Component {
     }
   }
 
-  // async componentDidMount() {
-  //   const { query } = this.props
-  //   console.log(query)
-  //   // one word query allowed
-  //   const results = await axios(`https://api.discogs.com/database/search?q=${query}&type=master&token=NcoQgYPGBIypBlMCtCHoHMAVWLIhKAMzSXKfBYan`)
-  //   this.setState({ results })
-  // }
-
   render () {
     let {results} = this.props
     if(results) {
       results = results.data.results.map(result => {
         const {id, title, cover_image} = result
-
-        console.log(id, title, cover_image)
         return { id, title, cover_image }
       })
       results = results.map(result => (
@@ -47,36 +37,8 @@ class Results extends Component {
       ))
     }
 
-    // const SearchBar = ({query, onChangeQuery, onSearch}) => (
-    //   <form className="form-inline my-2 my-lg-0">
-    //     <div className='mx-auto'>
-    //       <input
-    //         autoFocus
-    //         value={query}
-    //         onChange={onChangeQuery}
-    //         className="form-control rounded-0 btn-outline-dark"
-    //         type="search"
-    //         placeholder="Search Albums"
-    //         aria-label="Search"
-    //       />
-    //
-    //       <button className="rounded-0 btn btn-outline-dark my-2 my-sm-0" type="submit">
-    //         <FontAwesomeIcon
-    //           onClick={onSearch}
-    //           title="Search"
-    //           color='black'
-    //           icon={['fas', 'search']}
-    //         />
-    //       </button>
-    //     </div>
-    //   </form>
-    // )
-    //
-    // const {onSearch, query, onChangeQuery} = this.props
-
     return (
       <div className="container-fluid mt-5">
-        {/* <SearchBar onSearch={onSearch} query={query} onChangeQuery={onChangeQuery} /> */}
         <div className="d-flex flex-wrap justify-content-center">
           {results}
         </div>
