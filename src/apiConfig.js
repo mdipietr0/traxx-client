994d0f6af1eae734d8e358b920e9d79e21658ff0
mdipietr0 <dipietro.michael@gmail.com>
2018-10-10 10:23:42 -0400

    Remove trailing slash from production URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://protected-sands-90386.herokuapp.com/',
+  production: 'https://protected-sands-90386.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
