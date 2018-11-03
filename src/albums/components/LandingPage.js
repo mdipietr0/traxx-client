6f37cddae91bfe28a06e1c665ff5aff2d4fa52ba
mdipietr0 <dipietro.michael@gmail.com>
2018-11-03 15:09:45 -0400

    Fix columns on landing page to work on mobile

--- src/albums/components/LandingPage.js
+++ src/albums/components/LandingPage.js
@@ -13,15 +13,15 @@ const LandingPage = () => (
     </Jumbotron>
     <Container>
       <Row>
-        <Col>
+        <Col md="4">
           <h4 className='text-dark text-center'>Discover</h4>
           <p className='text-justify'>Find old favorites or discover new ones! Traxx uses the Discogs API to search over 10 million albums by over 5 million different artists.</p>
         </Col>
-        <Col>
+        <Col md="4">
           <h4 className='text-dark text-center'>Track</h4>
           <p className='text-justify'>You can create and share a wishlist and keep track of all your most wanted albums.</p>
         </Col>
-        <Col>
+        <Col md="4">
           <h4 className='text-dark text-center'>Share</h4>
           <p className='text-justify'>Your family and friends will know exactly what to get you!
             To begin creating your wishlist, <Link to='/sign-in'>Sign In</Link> or <Link to='/sign-up'>Create an Account</Link>!
